94074f710b56bc117cf8bc639b25f8e73f748fd8
PABlond <pierre.alexis.blond@gmail.com>
2019-10-22 16:57:48 +0200

    create routes for imagesearch & latest

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,8 +2,12 @@ import express from 'express'
 
 const Router = express.Router()
 
-Router.use('/', (req, res) => {
-    res.json('APDZQDQZI')
+Router.use('/imagesearch/:search', (req, res) => {
+    res.json(req.params)
+})
+
+Router.use('/latest/imagesearch/', (req, res) => {
+    res.json("")
 })
 
 export default Router
