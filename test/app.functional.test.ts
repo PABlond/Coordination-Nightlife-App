140215ae4a3fb761cbf352e21ada9beb0760d756
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-23 12:01:50 +0200

    improve functional tests

--- test/app.functional.test.ts
+++ test/app.functional.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest"
 import app from "./../src/app"
 
+const search = "lolcats"
+
 describe("Test the root path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/")
@@ -10,15 +12,17 @@ describe("Test the root path", () => {
 })
 
 describe("Test the /imagesearch/:search path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/api/imagesearch/lolcats")
+  test("Without offset", async () => {
+    const response = await request(app).get(`/api/imagesearch/${search}`)
     expect(response.status).toBe(201)
     expect(response.body).toBeDefined()
     expect(response.body.length).toBeGreaterThanOrEqual(0)
   })
 
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/api/imagesearch/lolcats?offset=11")
+  test("With offset", async () => {
+    const response = await request(app).get(
+      `/api/imagesearch/${search}?offset=11`
+    )
     expect(response.status).toBe(201)
     expect(response.body).toBeDefined()
     expect(response.body.length).toBeGreaterThanOrEqual(0)
@@ -29,5 +33,9 @@ describe("Test the /latest/imagesearch/ path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/api/latest/imagesearch/")
     expect(response.status).toBe(201)
+    expect(response.body.length).toBeLessThanOrEqual(10)
+    expect(Object.keys(response.body[0])).toEqual(
+      expect.arrayContaining(["term", "when"])
+    )
   })
 })
