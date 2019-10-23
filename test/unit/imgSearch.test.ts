2e893a9ba4e5a21e933bb9431ec51f416fff6b10
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-23 12:02:17 +0200

    add unit tests

--- test/unit/imgSearch.test.ts
+++ test/unit/imgSearch.test.ts
@@ -1,4 +1,5 @@
 import ImgSearch from "../../src/controllers/imgSearch"
+import mongoose from "mongoose"
 
 const imgSearch = new ImgSearch()
 
@@ -14,8 +15,33 @@ describe("Test searchWithParams", () => {
     const search = "cats"
     const start = "1"
     const response = await imgSearch.searchWithParams({ search, start })
+    expect(response.length).toBeGreaterThanOrEqual(0)
     expect(Object.keys(response[0])).toEqual(
       expect.arrayContaining(["url", "snippet", "context", "thumbnail"])
     )
+  })
+})
+
+describe("Test getLastSearch", () => {
+  test("It should return an array", async () => {
+    const response = await imgSearch.getLastSearch()
+    expect(response.length).toBeLessThanOrEqual(10)
+  })
+})
+
+describe("Test organizeLastSearch", () => {
+  test("It should return an array", async () => {
+    const response = imgSearch.organizeLastSearch([{_id: "aSampleId", term: "term", when: new Date()}])
+    expect(Object.keys(response[0])).toEqual(
+      expect.arrayContaining(["term", "when"])
+    )
+    expect(response.length).toBe(1)
+  })
+})
+
+beforeAll(async () => {
+  const { MONGO_PASSWORD, MONGO_USER, MONGO_DB } = process.env
+  mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_DB}`, {
+    useNewUrlParser: true
   })
 })
