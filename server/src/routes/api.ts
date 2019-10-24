74d5803c3e30fa84a376cf3d34f7626aae48ed15
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-24 12:06:21 +0200

    create routes for  businesses

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,12 +1,27 @@
 import express from "express"
-import Auth from "./../controllers/auth"
+import controllers from "./../controllers/"
+
+const { Auth, Businesses } = controllers
 
 const Router = express.Router()
 
 const auth = new Auth()
+const businesses = new Businesses()
 
 Router.post("/github-cb", async (req, res) => {
   return res.status(201).json(await auth.githubAuth(req.body.code))
+})
+
+Router.post("/search", async (req, res) => {
+  const { city } = req.body
+
+  return res.status(201).json(await businesses.getBusinesses(city))
+})
+
+Router.post("/business", async (req, res) => {
+  const { id } = req.body
+
+  return res.status(201).json(await businesses.getBusiness(req.body))
 })
 
 export default Router
