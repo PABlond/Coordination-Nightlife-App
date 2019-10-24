9f88f5475a0b9d565ba984ede105e5f5c84faae6
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-24 16:12:27 +0200

    fix TypeScript issues

---
+++ server/src/interfaces/query.interface.ts
@@ -0,0 +1,33 @@
+interface Hours {
+  is_open_now: Boolean
+}
+
+export interface ReqReview {
+  rating: Number
+  text: string
+  time_created: string
+  user: { image_url: string 
+    name: string }
+}
+
+export interface Review {
+  name: string
+  rating: Number
+  text: string
+  time_created: string
+}
+
+export interface IBuisness {
+  name: string
+  rating: string
+  photos: string[]
+  phone: string
+  location: {
+    display_address: String[]
+  }
+  coordinate: {
+    latitude: Number
+    longitude: Number
+  }
+  reviews: Review[]
+}
