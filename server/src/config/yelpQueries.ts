34a7eba30840012ecd559c22a0596e7798957628
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-24 12:05:17 +0200

    create a query to get all buisnesses

---
+++ server/src/config/yelpQueries.ts
@@ -0,0 +1,32 @@
+import gql from "graphql-tag"
+
+export const searchBusinesses = gql`
+  query SearchBusinesses($location: String) {
+    search(term: "bars", location: $location, limit: 5) {
+      business {
+        id
+        name
+        photos
+        location {
+          address1
+          postal_code
+          city
+          # formatted_address
+        }
+        hours {
+          # open {
+          #   is_overnight
+          #   end
+          #   start
+          #   day
+          # }
+          is_open_now
+        }
+        coordinates {
+          latitude
+          longitude
+        }
+      }
+    }
+  }
+`
