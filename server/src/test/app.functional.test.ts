32547dd8dd0e31757d4dc1f032d1f86969fc595b
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-24 12:06:34 +0200

    add functional tests for businesses

--- server/src/test/app.functional.test.ts
+++ server/src/test/app.functional.test.ts
@@ -9,3 +9,25 @@ describe("Test the root path", () => {
     expect(response.status).toBe(201)
   })
 })
+
+describe("Test the /api/search path", () => {
+  test("It should response with an array of buisnnesses", async () => {
+    const city = "Paris"
+    const response = await request(app)
+    .post("/api/search")
+    .send({city})
+    expect(response.status).toBe(201)
+    expect(response.body[0].location.city).toBe(city)
+  })  
+})
+
+describe("Test the /api/business path", () => {
+  test("It should response with an object of buisnness", async () => {
+    const id = "PX1hwexNzzxd4-2HR-utUg"
+    const response = await request(app)
+    .post("/api/business")
+    .send({id})
+    expect(response.status).toBe(201)
+    expect(response.body.id).toBe(id)
+  })  
+})
