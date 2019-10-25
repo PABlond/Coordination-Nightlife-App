9f8ba178dc1d61e695a8660916d8404f66bcff93
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-25 15:23:53 +0200

    add name mechanism to auth controller

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -23,10 +23,17 @@ export default class Auth {
   }
 
   reqEmail = async ({ access_token }: { access_token: string }) => {
-    const { data } = await axios.get(
+    const {
+      data: [{ email }]
+    } = await axios.get(
       `https://api.github.com/user/emails?access_token=${access_token}`
     )
-    return data[0].email as any
+    const {
+      data: { name }
+    } = await axios.get(
+      `https://api.github.com/user?access_token=${access_token}`
+    )
+    return { email, name }
   }
 
   isUserExists = async ({ email }: { email: string }) => {
@@ -37,21 +44,23 @@ export default class Auth {
     return User.findOne({ email })
   }
 
-  createUser = async ({ email }: { email: string }) => {
-    await new User({ email }).save()
+  createUser = async ({ email, name }: { email: string; name: string }) => {
+    await new User({ email, name }).save()
   }
 
-  getToken = (email: string) => jwt.sign({ email }, this.jwt_secret)
+  getToken = ({ email, name }: { email: string; name: string }) =>
+    jwt.sign({ email }, this.jwt_secret)
 
   decodeToken = (token: string) => jwt.verify(token, this.jwt_secret)
 
   githubAuth = async (code: string) => {
     const access_token = await this.reqAccessToken({ code })
-    const email = await this.reqEmail({ access_token })
-    if (!this.isUserExists({ email })) {
-      await this.createUser({ email })
+    const { email, name } = await this.reqEmail({ access_token })
+    if (!(await this.isUserExists({ email }))) {
+      await this.createUser({ email, name })
     }
-    const token = this.getToken(email)
+    // console.log(this.isUserExists({ email }))
+    const token = this.getToken({ email, name })
     return { token }
   }
 }
