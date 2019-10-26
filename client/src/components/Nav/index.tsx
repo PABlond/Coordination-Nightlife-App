b0f924466224db19521988ea6756d2bdd0b85159
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-26 11:21:23 +0200

    handle user info in nav

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,21 +1,44 @@
-import React from "react"
-import { Nav } from "react-bootstrap"
+import React, { useEffect, useState } from "react"
+import Auth from "./../../actions/auth"
 import { FaGithub } from "react-icons/fa"
 import styled from "styled-components"
+import { Container, Button } from "react-bootstrap"
 
 export default () => {
-  return (
-    <>
+  const [user, setUser] = useState<any>({ isLogged: false, username: "" })
+
+  const getUser = async () => {
+    const user = await new Auth().user()
+    console.log(user)
+    setUser({ isLogged: true, username: user.name })
+  }
+
+  const logout = () => {
+    new Auth().logout()
+    setUser({ isLogged: false, username: "" })
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+  return user.isLogged ? (
+    <Container fluid>
+      <MenuItem>
+        {user.username} <Button onClick={logout}> Logout</Button>
+      </MenuItem>
+    </Container>
+  ) : (
+    <Container fluid>
       <a
         href="https://github.com/login/oauth/authorize?scope=user:email&client_id=4e78567ab3cf78bb6571"
-        alt="github_login"
+        title="github_login"
       >
-        <MenuItem onClick={login}>
+        <MenuItem>
           Login
           <GithubLogin size="2em" />
         </MenuItem>
       </a>
-    </>
+    </Container>
   )
 }
 
