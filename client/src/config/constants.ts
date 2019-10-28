67318beaa72dcae5f0ace0da4752427484af119d
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-28 10:44:51 +0100

    create constant file

---
+++ client/src/config/constants.ts
@@ -0,0 +1,8 @@
+const api_url = "http://localhost:3000/api"
+
+export const routes = {
+  getBusiness: `${api_url}/business`,
+  placeEvent: `${api_url}/rsvp`,
+  search: `${api_url}/search`,
+  login: `${api_url}/github-cb`,
+}
